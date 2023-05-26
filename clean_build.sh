04a0ec7745a93a0f10ae19fc297d91fe107cb9e7
Eren Nevin <eren.m.nevin@gmail.com>
2023-05-26 06:21:39 +0300

    fix: clean_build.sh was fixed with the updated canister name

--- clean_build.sh
+++ clean_build.sh
@@ -2,8 +2,8 @@
 # Make these two commands as option
 dfx stop
 dfx start --clean --background --host 127.0.0.1:4999
-dfx canister create ProxyManager_backend
+dfx canister create Xtreamly_Canister_backend
 dfx build
-rm ./target/wasm32-unknown-unknown/release/ProxyManager_backend.wasm.gz
-gzip -f  ./target/wasm32-unknown-unknown/release/ProxyManager_backend.wasm
-dfx canister install ProxyManager_backend --upgrade-unchanged  --mode=reinstall  --wasm ./target/wasm32-unknown-unknown/release/ProxyManager_backend.wasm.gz -y
+rm ./target/wasm32-unknown-unknown/release/Xtreamly_Canister_backend.wasm.gz
+gzip -f  ./target/wasm32-unknown-unknown/release/Xtreamly_Canister_backend.wasm
+dfx canister install Xtreamly_Canister_backend --upgrade-unchanged  --mode=reinstall  --wasm ./target/wasm32-unknown-unknown/release/Xtreamly_Canister_backend.wasm.gz -y
