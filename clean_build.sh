5bae81f86d67ee0d43f70e9b09d778cccd0f97bf
Eren Nevin <eren.m.nevin@gmail.com>
2023-05-27 05:39:54 +0300

    add:  Added a shell script (change_network.sh) using sed substituion command to change the URL and CHAIN in consts.rs file. This is used to point to our test network or revert back to etheruem mainnet enhanced: Enhanced clean_build to get icp_port as well as calling change_network to tear down and recreate the whole icp

--- clean_build.sh
+++ clean_build.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
-# Make these two commands as option
+# Usually 4943
+icp_port="$1"
+ganache_host="$2"
+ganache_networkId="$3"
+
 dfx stop
-dfx start --clean --background --host 127.0.0.1:4999
+dfx start --clean --background --host "127.0.0.1:$icp_port"
 dfx canister create Xtreamly_Canister_backend
+
+./change_network.sh "$ganache_host" "$ganache_networkId"
+
 dfx build
 rm ./target/wasm32-unknown-unknown/release/Xtreamly_Canister_backend.wasm.gz
 gzip -f  ./target/wasm32-unknown-unknown/release/Xtreamly_Canister_backend.wasm
